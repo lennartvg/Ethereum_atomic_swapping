7108b9e8ae0cbfc3f905bec9979593e5b9df0813
clvang000 <clvangijtenbeek@gmail.com>
2018-01-17 13:35:06 +0100

    changed case 3

--- generalpurpose_contracts/3_crosschain_coin_swap.sol
+++ generalpurpose_contracts/3_crosschain_coin_swap.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.4.18;
 
 contract CrosschainCoinSwap {
     
-    address clientA_addr;
-    address clientB_addr;
+    address clientA;
+    address clientB;
     bytes32 hashed_secret;
     uint timeOut;
     
-    function CrosschainCoinSwap(address _clientA_addr, address _clientB_addr, bytes32 _hashed_secret) public {
-        clientA_addr = _clientA_addr;
-        clientB_addr = _clientB_addr;
+    function CrosschainCoinSwap(address _clientA, address _clientB, bytes32 _hashed_secret) public {
+        clientA = _clientA;
+        clientB = _clientB;
 		hashed_secret = _hashed_secret;
         timeOut = now + 1 hours;
     }
@@ -20,7 +20,7 @@ contract CrosschainCoinSwap {
 
     function claim(string _secret) public returns (bool) {
        if (hashed_secret == sha256(_secret)){
-            selfdestruct(clientB_addr);
+            selfdestruct(clientB);
             return true;
        } else {
             return false;
@@ -28,8 +28,8 @@ contract CrosschainCoinSwap {
     }
 
     function refund() public returns (bool) {
-        if (msg.sender == clientA_addr && now >= timeOut) {
-            selfdestruct(clientA_addr);
+        if (msg.sender == clientA && now >= timeOut) {
+            selfdestruct(clientA);
             return true;
         } else {
             return false;
